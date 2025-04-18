50dc3a132998197c17d7025770ef0f86f7b799c4
Gabriel Viana Santos <gabriel.viana.gv02@gmail.com>
2025-04-18 14:13:13 -0300

    Inicio do projeto
    Projeto Fintech iniciado.

---
+++ coinmapping/src/main/java/br/com/fiap/coinmapping/CoinmappingApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.coinmapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinmappingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinmappingApplication.class, args);
+	}
+
+}
