57cf8b32f14e54c33622a168f3a472d0b2fd4ec6
Gabriel Viana Santos <gabriel.viana.gv02@gmail.com>
2025-04-18 15:24:59 -0300

    Classe Dao Adicionada

---
+++ coinmapping/src/main/java/br/com/fiap/coinmapping/domain/reserva/ReservaDao.java
@@ -0,0 +1,22 @@
+package br.com.fiap.coinmapping.domain.reserva;
+
+import br.com.fiap.coinmapping.domain.reserva.Reserva;
+
+import java.sql.Connection;
+import java.util.List;
+
+public class ReservaDao {
+  private Connection conexao;
+  public void cadastrar(Reserva reserva){
+  }
+  public Reserva pesquisar(long id){
+    return null;
+  }
+  public List<Reserva> listar(){
+    return null;
+  }
+  public void atualizar(Reserva reserva){
+  }
+  public void remover(long id){
+  }
+}
--- coinmapping/src/main/java/br/com/fiap/coinmapping/domain/user/UserDao.java
+++ coinmapping/src/main/java/br/com/fiap/coinmapping/domain/user/UserDao.java
@@ -1,1 +1,19 @@
+package br.com.fiap.coinmapping.domain.user;
+import java.sql.Connection;
+import java.util.List;
 
+public class UserDao {
+  private Connection conexao;
+  public void cadastrar(User usuario){
+  }
+  public User pesquisar(long id){
+    return null;
+  }
+  public List<User> listar(){
+    return null;
+  }
+  public void atualizar(User usuario){
+  }
+  public void remover(long id){
+  }
+}
